df0f9b7de04324ab64d7d9722f6d6efe0cfab991
Drigues <miguel.rodrigues103@hotmail.com>
2026-05-27 16:52:01 +0100

    terraform backend

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,11 +1,4 @@
-/*
-  Backend configuration example. For production use, configure an S3 backend
-  with DynamoDB for state locking. This file is intentionally left as an
-  example and commented out so `terraform init` will not fail.
-
-  Example:
-
-  terraform {
+terraform {
     backend "s3" {
       bucket         = "my-terraform-state-bucket"
       key            = "project/terraform.tfstate"
@@ -14,5 +7,3 @@
       encrypt        = true
     }
   }
-*/
-
